814743358df1ec2a07352a67d0711ff0eb92a98a
徐瑞骏 <1687701765@qq.com>
2026-04-05 22:05:16 +0800

    perf: 优化ADC数据处理, Modbus能成功读取到数据

--- User/Src/dcdc.c
+++ User/Src/dcdc.c
@@ -21,6 +21,7 @@
 #include "mos_pwm.h"
 #include "py32f0xx_hal.h"
 
+#define PROT_EN  0
 #define CV_EN    1
 #define CC_EN    0
 #define MPPT_EN  0
@@ -85,49 +86,52 @@ void DCDC_Soft_Start()
 
 void DCDC_ADC_update_callback(ADCSamp_t *data)
 {
-  int32_t pwm_value = -1;
+  int32_t pwm_value = 0;
   //TODO: STOP模式添加电压监控，从STOP模式启动
   //VBUS持续供电MCU一直处于运行，但DCDC没有电源输入不运行，日出光伏板接受光照时需要自动启动
+#if PROT_EN
   if((data->vbus > param.v_prot) || (data->ibus > param.i_prot)){
     mode = DCDC_Mode_Stop;
-  }
+  }else
+#endif
+
 #if CV_EN
-  else if(mode == DCDC_Mode_CV){
+  if(mode == DCDC_Mode_CV){
 #if CC_EN
     if((data->ibus > param.cc_targ) && (data->vbus < param.cv_targ)){
       mode = DCDC_Mode_CC;
       uint32_t old_pwm = MOSPWM_GetOutputCompare();
       PI_set_s(&pi_cc, data->ibus, old_pwm);
+      pwm_value = old_pwm;
     }else{
       pwm_value = PI_update(&pi_cv, data->vbus);
     }
 #else
     pwm_value = PI_update(&pi_cv, data->vbus);
 #endif
-  }
+  }else
 #endif
 
 #if CC_EN
-  else if(mode == DCDC_Mode_CC){
+  if(mode == DCDC_Mode_CC){
 #if CV_EN
     if((data->ibus < param.cc_targ) && (data->vbus > param.cv_targ)){
       mode = DCDC_Mode_CV;
       uint32_t old_pwm = MOSPWM_GetOutputCompare();
       PI_set_s(&pi_cv, data->vbus, old_pwm);
+      pwm_value = old_pwm;
     }else{
       pwm_value = PI_update(&pi_cc, data->ibus);
     }
 #else
     pwm_value = PI_update(&pi_cc, data->ibus);
 #endif
-  }
+  }else
 #endif
 
-  else{
-    pwm_value = 0;
+  {
+  //  pwm_value = 0;
   }
-  if(pwm_value >= 0){
-    MOSPWM_SetOutputCompare(192-pwm_value);
-  }
+  MOSPWM_SetOutputCompare(192-pwm_value);
   update_count++;
 }
